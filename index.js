eb2e1bd58e0d9a92f9d76912c9a2d57117116704
Fahad Malik <fahadrazaqmalik@gmail.com>
2023-06-19 06:58:11 +0200

    changes to the code

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 class User {
 
+    message;
+    username;
+
     constructor(){
         this.message = 'Hello World' ;
         console.log(this.messge);
     }
 
-    getUserName(){}
+    setUsername(name) {
+        this.username = name;
+    }
+
+    getUserName(){
+        return this.username = name;
+    }
+
+    getUserName () {}
 
     getUserStatus(){}
 
