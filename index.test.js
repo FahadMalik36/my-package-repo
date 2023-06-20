1f10a85856e0c5d3d47554ba408c703fe4bd285d
Fahad Malik <fahadrazaqmalik@gmail.com>
2023-06-20 15:52:34 +0200

    Revert "tests"
    This reverts commit 53eaba1b7661cf81f92dc9f1c9375b8602ddc214.

--- index.test.js
+++
@@ -1,5 +0,0 @@
-const { exportAllDeclaration } = require("@babel/types");
-const getURLByLocation = require("./../index");
-test("Returns user to located website by country ", () => {
-    expect (getURLByLocation("US")).toBe("/home");
-});
